ac82e3d3c6369fa6ae41732c40c0884d3e0432e4
Your Name <skyblack1401@gmail.com>
2018-11-15 12:44:47 +0700

    fix migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('fullname')->nullable();
             $table->string('status')->nullable();
-            $table->string('email')->unique();
+            $table->string('email', 250)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
