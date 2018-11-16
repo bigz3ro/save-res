a3af0c40d2d0ffb801260243cb02997844e66b3c
Your Name <skyblack1401@gmail.com>
2018-11-16 15:25:22 +0700

    fix color button

--- resources/views/pages/organizations/index.blade.php
+++ resources/views/pages/organizations/index.blade.php
@@ -15,7 +15,7 @@
           <div class="input-group input-group-sm" style="width: 550px;">
             <input type="text" name="keyword" class="form-control pull-right" placeholder="Search">
             <div class="input-group-btn">
-              <button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
+              <button type="submit" class="btn btn-info"><i class="fa fa-search"></i></button>
             </div>
           </div>
         </div>
